7e5a4e1f42b4e54dbd5daa62d40fde4cea6041c5
Ramin Taghizada <ramintagizade@mail.ru>
2018-03-05 17:44:03 +0400

    rush graphics & platforms

--- js/game.js
+++ js/game.js
@@ -1,3 +1,54 @@
+class GameObject extends createjs.Container {
+	constructor(graphic){
+		super();
+		if(graphic !== undefined){
+			this.graphic = graphic;
+			this.addChild(this.graphic);
+			var b = this.graphic.nominalBounds;
+			this.setBounds(b.x,b.y,b.width,b.height);
+		}
+	}
+}
+class Hero extends GameObject {
+	constructor() {
+		super(new lib.HeroGraphic());
+	}
+}
+class Platform extends GameObject {
+	constructor() {
+		super(new lib.PlatformGraphic());
+	}
+}
+class World extends createjs.Container {
+	constructor() {
+		super();
+
+		// store platforms 
+		this.platforms = [];
+		this.generatePlatforms();
+		this.addHero();
+	}
+	addHero() {
+		var hero = new Hero();
+		hero.x = 100;
+		hero.y = 80;
+		this.addChild(hero);
+	}
+	generatePlatforms(){
+		var platform = new Platform();
+		platform.x = 100;
+		platform.y = 100;
+		this.platforms.push(platform);
+		this.addChild(platform);
+
+		// 2nd platform 
+		platform = new Platform();
+		platform.x = 200;
+		platform.y = 200;
+		this.platforms.push(platform);
+		this.addChild(platform);
+	}
+}
 class Game {
 	constructor() {
 		console.log(`Welcome to the game .Version ${this.version()}`);
@@ -20,7 +71,7 @@ class Game {
 		// keep re-drawing 
 		createjs.Ticker.on("tick",this.stage); 
 		
-		this.restartGame();
+		this.loadGraphics();
 		
 	}
 	version() {
@@ -29,8 +80,28 @@ class Game {
 	loadSound() {
 		
 	}
+	loadGraphics(){
+		var loader = new createjs.LoadQueue(false);
+		loader.addEventListener("fileload",handleFileLoad);
+		loader.addEventListener("complete",handleComplete.bind(this));
+		loader.loadFile({src:"images/rush_game_graphics_atlas_.json",
+		type:"spritesheet", id:"rush_game_graphics_atlas_"},true);
+		loader.loadManifest(lib.properties.manifest);
+
+		function handleFileLoad(evt){
+			if(evt.item.type === "image" ){
+				images[evt.item.id] = evt.result;
+			}
+		}
+		function handleComplete(evt) {
+			var queue = evt.target;
+			ss["rush_game_graphics_atlas_"] = queue.getResult("rush_game_graphics_atlas_");
+			this.restartGame();
+		}
+	}
 	restartGame() {
-		
+		var world = new World();
+		this.stage.addChild(world);
 	}
 	
 	retinalize() {
